e45c6609befca99a03273bf1a2036e6dddad8063
Craig Snowden <craig@craigsnowden.com>
2015-10-18 01:15:50 +0100

    Add cards controller. Going to need to specify the customer output in JBuilder as we probably want to list a customers cards.

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,32 @@
+class CardsController < ApplicationController
+  respond_to :json
+  def index
+    @cards = Card.all
+    respond_with @cards
+  end
+
+  def show
+    @card = Card.find(params[:id])
+    respond_with @card
+  end
+
+  def create
+    @card = Card.create(card_params)
+    respond_with @card
+  end
+
+  def update
+    @card = Card.find(params[:id]).update_attributes(card_params)
+    respond_with @card
+  end
+
+  def destroy
+    @card = Card.find(params[:id]).destroy
+    respond_with @card
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:id, :customer_id)
+    end
+end
