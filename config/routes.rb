ebb88d51880c3b07a3f2cafa5931b017bbdb6817
Craig Snowden <craig@craigsnowden.com>
2015-10-23 23:07:32 +0100

    Hopefully we can avoid having multiple Pumas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :customers
   resources :cards
+  match "/websocket", :to => ActionCable.server, via: [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
