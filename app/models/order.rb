4314969907bdc98257b1e64b1ee190a0cb6e5cb2
Craig Snowden <craig@craigsnowden.com>
2015-11-18 01:57:13 +0000

    Lets stream stuff about tables and orders. Why not.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :card
   belongs_to :table
+  after_save :stream_update
+
+  private
+    def stream_update
+      ActionCable.server.broadcast 'tables',
+        table_id: self.table.id,
+        order_id: self.id,
+        status: self.status
+    end
 end
