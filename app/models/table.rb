4014396c51f06c220b444907cce1e7ef22de76fd
Craig Snowden <craig@craigsnowden.com>
2015-11-23 22:51:33 +0000

    Wow my overlapping logic was shit

--- app/models/table.rb
+++ app/models/table.rb
@@ -3,6 +3,6 @@ class Table < ActiveRecord::Base
   has_many :orders
 
   def free_at(start_time)
-    Occupancy.where("start >= ?", start_time).where("end <= ? OR end = NULL", start_time + 2.hours).count == 0
+    occupancies.where("(start >= ? AND end <= ?) OR ? > start", start_time, start_time, start_time + 2.hours).count == 0
   end
 end
