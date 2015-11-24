a13025d4c5439683642abda68c64c0a8ddf842ad
Craig Snowden <craig@craigsnowden.com>
2015-11-24 17:28:49 +0000

    Stupid s

--- app/models/occupancy.rb
+++ app/models/occupancy.rb
@@ -1,7 +1,7 @@
 class Occupancy < ActiveRecord::Base
   belongs_to :customer
   belongs_to :table
-  validates :only_one_occupied_occupancy_per_table
+  validate :only_one_occupied_occupancy_per_table
 
   def only_one_occupied_occupancy_per_table
     if occupied && table.occupancies.where(occupied: true).count > 0
