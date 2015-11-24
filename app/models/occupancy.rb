e35465701c5f5375a03723bd894331790d82c3ad
Craig Snowden <craig@craigsnowden.com>
2015-11-24 17:20:39 +0000

    Validate on whether a table is occupied by another booking

--- app/models/occupancy.rb
+++ app/models/occupancy.rb
@@ -1,4 +1,11 @@
 class Occupancy < ActiveRecord::Base
   belongs_to :customer
   belongs_to :table
+  validates :only_one_occupied_occupancy_per_table
+
+  def only_one_occupied_occupancy_per_table
+    if occupied && table.occupancies.where(occupied: true).count > 0
+      errors.add(:occupied, "table is already occupied")
+    end
+  end
 end
