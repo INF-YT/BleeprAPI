5a24918e26fcc46917472d64cb15b85d18466c19
Craig Snowden <craig@craigsnowden.com>
2015-11-24 16:07:31 +0000

    Remove null from the tables array

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -35,7 +35,7 @@ class TablesController < ApplicationController
 
   def occupied
     @occupancies = Occupancy.where(occupied: true)
-    @tables = {"tables" => @occupancies.pluck(:table_id).uniq}
+    @tables = {"tables" => @occupancies.pluck(:table_id).compact}
     render json: @tables
   end
 
